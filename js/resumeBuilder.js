820a05c6baaa0bac69147dd88ddfe856dad27064
xgame92 <x_game92@hotmail.com>
2016-03-26 22:58:25 +0200

    commited

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,2 +1,24 @@
-$("#main").append(["yiğit Tanrıverdi"]);
-var awesomeThoughts  = "I am Yiğit and this is AWEOSOME";
+
+var name = "Yiğit";
+var role = "Sofware Engineer";
+
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+var skills = ["awesomeness", "programing", "teaching","JS"];
+
+var bio = {
+  name : "Yiğit",
+  role : "Sofware Engineer",
+  contact : "izmir",
+  picture_url : "www.yigittanriverdi.com",
+  welcome_message : "wellcome to my page"
+}
+//$("#header").append(skills);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").append()
+console.log(HTMLheaderName);
+console.log(formattedRole);
+ai@
