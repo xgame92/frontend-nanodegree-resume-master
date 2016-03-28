0be3642ec989df58535a505618cc71c6b9a89ce1
xgame92 <x_game92@hotmail.com>
2016-03-28 22:23:09 +0300

    phpstorm Commit

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -8,87 +8,87 @@ $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
 document.getElementById('education').style.display = "none";
 
-if (quiz == true){
-  $("#header").prepend(skillStart);
-}else {
+if (quiz == true) {
+    $("#header").prepend(skillStart);
+} else {
 
 }
 
 var work = {
-    "job" : [{
-      "employer": "ali",
-      "title": "alinin işi",
-      "location": "Türkiye",
-      "dates": "04.04.2017",
-      "description": "alinin işi çok iyi"
+    "job": [{
+        "employer": "ali",
+        "title": "alinin işi",
+        "location": "Türkiye",
+        "dates": "04.04.2017",
+        "description": "alinin işi çok iyi"
     }, {
-      "employer": "veli",
-      "title": "velinin işi",
-      "location": "Almanya",
-      "dates": "03.03.2017",
-      "description": " bu veli çok çalışkan"
+        "employer": "veli",
+        "title": "velinin işi",
+        "location": "Almanya",
+        "dates": "03.03.2017",
+        "description": " bu veli çok çalışkan"
     }]
 
 }
 
 var projects = {
 
-    "project" : {
-      "title": "alinin projesi",
-      "dates": "03.02.2017",
-      "worked": 15,
-      "description": "nasıl bi proje bu ya açıklama yok",
-      "images": ["buraya proje resimi gelecek", "2. proje resimi"]
+    "project": {
+        "title": "alinin projesi",
+        "dates": "03.02.2017",
+        "worked": 15,
+        "description": "nasıl bi proje bu ya açıklama yok",
+        "images": ["buraya proje resimi gelecek", "2. proje resimi"]
     },
-    "project"  :{
-      "title": "alinin projesi",
-      "dates": "03.02.2017",
-      "worked": 15,
-      "description": "nasıl bi proje bu ya açıklama yok",
-      "images": ["buraya proje resimi gelecek", "2. proje resimi"]
+    "project": {
+        "title": "alinin projesi",
+        "dates": "03.02.2017",
+        "worked": 15,
+        "description": "nasıl bi proje bu ya açıklama yok",
+        "images": ["buraya proje resimi gelecek", "2. proje resimi"]
     }
 
 }
 
 var bio = {
-  "name": "yiğit Tanrıverdi",
-  "role": "yazılımcı arkadaş",
-  "contacts": {
-    "mobile": null,
-    "email": "yigittanriverdi@yandex.com",
-    "github": "xgame92",
-    "twitter": "hloqer",
-    "location": "IZMIR"
-  },
-  "welcomeMessage": "Hoşgeldin yiğit usta",
-  "skills": [
-    "php", "javascript", "android"
-  ],
-  "bioPic": "url"
+    "name": "yiğit Tanrıverdi",
+    "role": "yazılımcı arkadaş",
+    "contacts": {
+        "mobile": null,
+        "email": "yigittanriverdi@yandex.com",
+        "github": "xgame92",
+        "twitter": "hloqer",
+        "location": "IZMIR"
+    },
+    "welcomeMessage": "Hoşgeldin yiğit usta",
+    "skills": [
+        "php", "javascript", "android"
+    ],
+    "bioPic": "url"
 }
 
 var education = {
-  "school": [{
-    "name": "Izmir University",
-    "location": "Turkey",
-    "degree": "Master",
-    "majors": ["CS02"],
-    "dates": 2017,
-    "url": "url"
-  }, {
-    "name": "Izmir University",
-    "location": "Turkey",
-    "degree": "Master",
-    "majors": ["CS02"],
-    "dates": 2017,
-    "url": "url"
-  }],
-  "onlineCourse": [{
-    "title": "Javascript Awesome Course",
-    "school": "Udacity",
-    "dates": 2016,
-    "url": "www.anonym.com"
-  }]
+    "school": [{
+        "name": "Izmir University",
+        "location": "Turkey",
+        "degree": "Master",
+        "majors": ["CS02"],
+        "dates": 2017,
+        "url": "url"
+    }, {
+        "name": "Izmir University",
+        "location": "Turkey",
+        "degree": "Master",
+        "majors": ["CS02"],
+        "dates": 2017,
+        "url": "url"
+    }],
+    "onlineCourse": [{
+        "title": "Javascript Awesome Course",
+        "school": "Udacity",
+        "dates": 2016,
+        "url": "www.anonym.com"
+    }]
 
 
 }
